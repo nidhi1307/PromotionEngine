e43df2d1ec17ef0d1ac46398bb4806517578f0ad
Nidhi Yadav <nidhiyadav@hashedin.com>
2020-07-23 00:34:02 +0530

    Pushing all the unit test cases

--- PromotionCalculatorTest/UnitTest.cs
+++ PromotionCalculatorTest/UnitTest.cs
@@ -23,5 +23,51 @@ namespace PromotionCalculatorTest
             Assert.AreEqual(100, Total);
 
         }
+        [TestMethod]
+        public void OneDiscount()
+        {
+            List<CartSKU> cartItems = new List<CartSKU> {
+                new CartSKU{SKUName="a",SKUQuantity=5},
+                new CartSKU{SKUName="b",SKUQuantity=5},
+                new CartSKU{SKUName="c",SKUQuantity=1}
+                };
+            PromotionCalculator PromoCalObj = new PromotionCalculator(new PromotionSKUObj());
+            int Total = PromoCalObj.GetCartTotalAmount(cartItems);
+
+            //Assert
+            Assert.AreEqual(370, Total);
+
+        }
+        [TestMethod]
+        public void AllDiscount()
+        {
+            List<CartSKU> cartItems = new List<CartSKU> {
+                new CartSKU{SKUName="a",SKUQuantity=3},
+                new CartSKU{SKUName="b",SKUQuantity=5},
+                new CartSKU{SKUName="c",SKUQuantity=1},
+                new CartSKU{SKUName="d",SKUQuantity=1}
+                };
+            PromotionCalculator PromoCalObj = new PromotionCalculator(new PromotionSKUObj());
+            int Total = PromoCalObj.GetCartTotalAmount(cartItems);
+
+            //Assert
+            Assert.AreEqual(280, Total);
+
+        }
+        [TestMethod]
+        public void NullValueEntry()
+        {
+            List<CartSKU> cartItems = new List<CartSKU> {
+                new CartSKU{SKUName="a",SKUQuantity=3},
+                new CartSKU{SKUName="b",SKUQuantity=5},
+                new CartSKU{SKUName="c",SKUQuantity=0},
+                new CartSKU{SKUName="d",SKUQuantity=1}
+                };
+            PromotionCalculator PromoCalObj = new PromotionCalculator(new PromotionSKUObj());
+            int Total = PromoCalObj.GetCartTotalAmount(cartItems);
+
+            //Assert
+            Assert.AreEqual(265, Total);
+        }
     }
 }
